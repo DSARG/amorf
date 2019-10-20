403ba0355abe7b6e7d252db8ef24a8837626f9c8
davidhildner <dhildner42@gmail.com>
2019-10-20 21:38:38 +0200

    added tests for dataset module

--- framework/datasets.py
+++ framework/datasets.py
@@ -1,8 +1,80 @@
-import numpy as np
 import arff
+import numpy as np
 
 
 class EDM():
+    def __init__(self):
+        with open('data/edm.arff') as file:
+            dataset = arff.load(file)
+            data = np.array(dataset['data'])
+            self.X = X = data[:, 0:16].astype(np.float32)
+            self.y = y = data[:, 16:18].astype(np.float32)
+
+    def get_numpy(self):
+        """Retrieve X and Y as Numpy Array
+
+        Returns tuple with two arrays, X and y
+
+        Args:
+
+        Returns:
+            (np.ndarray, np.ndarray): Numpy Arraya X and y of type np.float32
+
+        """
+        return self.X, self.y
+
+    def get_pytorch_dataloader(self):
+        """Retrieve X and Y as PyTorch dataLoaders
+
+        Returns dataloaders with two arrays, X and y
+
+        Args:
+
+        Returns:
+            (np.ndarray, np.ndarray): Two PyTorch dataloaders X and y
+
+        """
+        raise NotImplementedError
+
+
+class RiverFlow1():
+    def __init__(self):
+        with open('data/rf1.arff') as file:
+            dataset = arff.load(file)
+            data = np.array(dataset['data'])
+            self.X = data[:, 0:64].astype(np.float32)
+            self.y = data[:, 64:72].astype(np.float32)
+            self.X[self.X is None] = 0
+            self.y[self.y is None] = 0
+
+    def get_numpy(self):
+        """Retrieve X and Y as Numpy Array
+
+        Returns tuple with two arrays, X and y
+
+        Args:
+
+        Returns:
+            (np.ndarray, np.ndarray): Numpy Arraya X and y of type np.float32
+
+        """
+        return self.X, self.y
+
+    def get_pytorch_dataloader(self):
+        """Retrieve X and Y as PyTorch dataLoaders
+
+        Returns dataloaders with two arrays, X and y
+
+        Args:
+
+        Returns:
+            (np.ndarray, np.ndarray): Two PyTorch dataloaders X and y
+
+        """
+        raise NotImplementedError
+
+
+class WaterQuality():
     def __init__(self):
         with open('data/wq.arff') as file:
             dataset = arff.load(file)
@@ -18,7 +90,7 @@ class EDM():
         Args:
 
         Returns:
-            tuple: Two Numpy Array X and y of type np.float32
+            (np.ndarray, np.ndarray): Numpy Arraya X and y of type np.float32
 
         """
         return self.X, self.y
@@ -31,7 +103,7 @@ class EDM():
         Args:
 
         Returns:
-            tuple: Two PyTorch dataloaders X and y
+            (np.ndarray, np.ndarray): Two PyTorch dataloaders X and y
 
         """
         raise NotImplementedError
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -2,6 +2,60 @@
 # testing in general, but rather to support the `find_packages` example in
 # setup.py that excludes installing the "tests" package
 
+import unittest
 
-def test_success():
-    assert True
+import numpy
+
+import framework.datasets as datasets
+
+
+class TestEDMDataset(unittest.TestCase):
+
+    def test_getNumpy_dimensions(self):
+        X, y = datasets.EDM().get_numpy()
+        self.assertEqual(len(X), len(y))
+        self.assertEqual(len(X[0, :]), 16)
+        self.assertEqual(len(y[0, :]), 2)
+
+    def test_getNumpy_type(self):
+        X, y = datasets.EDM().get_numpy()
+        self.assertTrue(type(X) is numpy.ndarray)
+        self.assertTrue(X.dtype is numpy.dtype('float32'))
+        self.assertTrue(type(y) is numpy.ndarray)
+        self.assertTrue(y.dtype is numpy.dtype('float32'))
+
+
+class TestRF1Dataset(unittest.TestCase):
+
+    def test_getNumpy_dimensions(self):
+        X, y = datasets.RiverFlow1().get_numpy()
+        self.assertEqual(len(X), len(y))
+        self.assertEqual(len(X[0, :]), 64)
+        self.assertEqual(len(y[0, :]), 8)
+
+    def test_getNumpy_type(self):
+        X, y = datasets.RiverFlow1().get_numpy()
+        self.assertTrue(type(X) is numpy.ndarray)
+        self.assertTrue(X.dtype is numpy.dtype('float32'))
+        self.assertTrue(type(y) is numpy.ndarray)
+        self.assertTrue(y.dtype is numpy.dtype('float32'))
+
+
+class TestWQDataset(unittest.TestCase):
+
+    def test_getNumpy_dimensions(self):
+        X, y = datasets.WaterQuality().get_numpy()
+        self.assertEqual(len(X), len(y))
+        self.assertEqual(len(X[0, :]), 14)
+        self.assertEqual(len(y[0, :]), 16)
+
+    def test_getNumpy_type(self):
+        X, y = datasets.WaterQuality().get_numpy()
+        self.assertTrue(type(X) is numpy.ndarray)
+        self.assertTrue(X.dtype is numpy.dtype('float32'))
+        self.assertTrue(type(y) is numpy.ndarray)
+        self.assertTrue(y.dtype is numpy.dtype('float32'))
+
+
+if __name__ == '__main__':
+    unittest.main()
