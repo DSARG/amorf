f06c98557014e4990a30de9bd9216c61cd7367ef
davidhildner <dhildner42@gmail.com>
2019-10-23 21:24:28 +0200

    added template documentation

--- framework/metrics.py
+++ framework/metrics.py
@@ -16,8 +16,26 @@ def average_relative_error(y_test, y_pred):
 
 
 def average_relative_root_mean_squared_error(y_test, y_pred):
+    """[summary]
+    
+    Args:
+        y_test ([type]): [description]
+        y_pred ([type]): [description]
+    
+    Returns:
+        [type]: [description]
+    """
     return sum(sqrt(sum((y_test - y_pred)**2) / sum((y_test - mean(y_test, axis=0))**2))) / len(y_pred[0, :])
 
 
 def tensor_average_relative_root_mean_squared_error(y_pred, y_test):
+    """[summary]
+    
+    Args:
+        y_pred ([type]): [description]
+        y_test ([type]): [description]
+    
+    Returns:
+        [type]: [description]
+    """
     return t.sum(t.sqrt(t.sum((y_test - y_pred)**2, dim=0) / t.sum(((y_test - t.mean(y_pred, dim=0))**2), dim=0))) / len(y_pred[0, :])
