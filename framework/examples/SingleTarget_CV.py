0ac9ced5f8f5128a6f6acdf4aa50e3d020162871
davidhildner <dhildner42@gmail.com>
2019-11-19 15:34:51 +0100

    added Cross Validation Prototype for Single Target Methods

---
+++ framework/examples/SingleTarget_CV.py
@@ -0,0 +1,44 @@
+import framework.datasets as ds
+import framework.problemTransformation as pt 
+import framework.metrics as metrics
+import numpy as np
+from sklearn.model_selection import KFold
+
+edm = ds.EDM().get_numpy() 
+rf1 = ds.RiverFlow1().get_numpy() 
+wq = ds.WaterQuality().get_numpy() 
+transCond = ds.TransparentConductors().get_numpy() 
+
+datasets = [edm,rf1,wq,transCond] 
+results_datasets = []
+for dataset in datasets: 
+
+    selectors = ['linear', 'kneighbors',
+                          'adaboost', 'gradientboost', 'mlp', 'svr', 'xgb'] 
+    all_results = []
+    for selector in selectors: 
+        SM = pt.SingleTargetMethod(selector) 
+        X = dataset[0] 
+        y = dataset[1] 
+        kf = KFold(n_splits=5, random_state=1, shuffle=True) 
+        selector_results = []
+        for train_index, test_index in kf.split(X): 
+            prediction = SM.fit(X[train_index],y[train_index]).predict(X[test_index]) 
+            result = metrics.average_relative_root_mean_squared_error(prediction,y[test_index])
+            selector_results.append(result)  
+        
+        all_results.append(selector_results) 
+    means_and_std = []
+    for result in all_results: 
+        mean,std = np.mean(result), np.std(result) 
+        means_and_std.append([mean,std]) 
+
+    results_datasets.append(means_and_std)
+print(results_datasets)  
+for dataset in results_datasets: 
+    count = 0
+    print("DATASET")
+    for selector in dataset: 
+        print(selectors[count]) 
+        print("Mean {}\tStd Dev {}".format(selector[0],selector[1]))
+        count += 1
